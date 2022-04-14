7fdcc60b5fcebf8b666a58506193885fca44c029
Anthony Bacilio Ruiz <bacilio.anthony@gmail.com>
2022-04-13 21:04:56 -0500

    Feat: Express recon
    Express server was reconnected to our solution

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+import express, { Application, NextFunction, Request, Response } from "express";
+
+dotenv.config();
+const app: Application = express();
+app.use(express.json());
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    res
+        .status(err.statusCode || 500)
+        .json({ type: err.errorType, msg: err.message, status: false });
+});
+
+export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import dbConnection from "./config/mongoDbConfig";
 import dotenv from "dotenv";
+import app from "./app"
 
 dotenv.config();
 dbConnection(`${process.env.MONGO_URI}`);
+
+app.listen(process.env.PORT, () =>
+  console.log(`Express Server Connected to port ${process.env.PORT}`)
+);
